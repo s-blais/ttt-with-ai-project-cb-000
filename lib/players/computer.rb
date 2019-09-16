831892bf4ed60c89a73a3cb0514b62acedb3c5e7
Stephen Blais <stephen@s-blais.com>
2019-09-16 16:06:39 +0000

    Automatically backed up by Learn

--- lib/players/computer.rb
+++ lib/players/computer.rb
@@ -3,6 +3,17 @@ module Players
 
     attr_reader :board, :free_spaces
 
+    WIN_COMBINATIONS = [
+      [0,1,2], # top row
+      [3,4,5], # middle row
+      [6,7,8], # bottom row
+      [0,3,6], # first column
+      [1,4,7], # second column
+      [2,5,8], # third column
+      [0,4,8], # diagonal top to bottom
+      [2,4,6]  # diagonal bottom to top
+    ]
+
 
     def find_free_spaces(board)
       @free_spaces = []
@@ -19,7 +30,7 @@ module Players
     end
 
     def block_or_win?(board)
-      game.WIN_COMBINATIONS.each do |combo|
+      WIN_COMBINATIONS.each do |combo|
           if (combo.count{|i| board.cells[i] == "X"}) == 2
             combo.index(" ")
           elsif (combo.count{|i| board.cells[i] == "O"}) == 2
